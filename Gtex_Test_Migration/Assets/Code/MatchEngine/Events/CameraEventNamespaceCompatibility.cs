9616a9ad440daf71eeff6f3fca853fb119e12639
wandemcphill <vidzimedialtd@gmail.com>
2026-08-28 00:12:59 +0100

    revert accidental direct main camera shim

--- Gtex_Test_Migration/Assets/Code/MatchEngine/Events/CameraEventNamespaceCompatibility.cs
+++
@@ -1,9 +0,0 @@
-using FStudio.Events;
-
-namespace FStudio.MatchEngine.Cameras
-{
-    // Namespace compatibility shim for CameraSystem's existing event reference.
-    internal sealed class MatchCameraActiveEvent : IBaseEvent
-    {
-    }
-}
